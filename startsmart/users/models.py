d15c579a6efe66691ba1fc4cfac1b9cab9cd3a74
Steve Chung Hyun Kim <steveck@uci.edu>
2020-10-11 05:42:18 -0700

    added Campaign model

--- startsmart/users/models.py
+++ startsmart/users/models.py
@@ -10,3 +10,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+class Campaign(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    target_money = models.IntegerField()
+    current_money = models.IntegerField()
+    days_left = models.IntegerField()
+    num_of_backers = models.IntegerField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def imageUrl(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
