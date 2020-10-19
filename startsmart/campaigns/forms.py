083e0512f796a5a06742df994bd8d6e1a9fac532
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-10-19 10:39:47 -0700

    init

--- startsmart/campaigns/forms.py
+++ startsmart/campaigns/forms.py
@@ -5,7 +5,3 @@ class CampaignForm(forms.ModelForm):
     class Meta:
         model = Campaign
         fields = '__all__'
-
-        widgets = {
-                'user': forms.TextInput(attrs={'value': '', 'id': 'element', 'type': 'hidden'})
-            }
