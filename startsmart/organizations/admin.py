be1408c704b8729c872605aee5302cab200bad31
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-11-06 12:00:50 -0800

    added organization model, registered app

--- startsmart/organizations/admin.py
+++ startsmart/organizations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Campaign
+from .models import Organization
 
 # Register your models here.
 
-admin.site.register(Campaign)
+admin.site.register(Organization)
--- startsmart/organizations/models.py
+++ startsmart/organizations/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Organization(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizations')
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
