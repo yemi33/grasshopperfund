c9245cba6333ba7dc942b008c9d15c98737c58b8
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-11-06 11:55:03 -0800

    added base files for organization

---
+++ startsmart/organizations/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Organization(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
