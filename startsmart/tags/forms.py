907d66c94c9d60b8f11b52ddb34986152fb40668
possipov <possipov@uci.edu>
2020-11-10 21:26:59 -0800

    Commented out the form of Tags.

--- startsmart/tags/forms.py
+++ startsmart/tags/forms.py
@@ -1,19 +1,19 @@
-from django import forms
-
-from .models import Tags
-from startsmart.campaigns.models import Campaign
-
-class TagsForm(forms.ModelForm):
-    campaign = forms.ChoiceField(choices=Campaign.objects.all())
-
-    def __init__(self, *args, **kwargs):
-        super(TagsForm, self).__init__(*args, **kwargs)
-
-    class Meta:
-        model = Tags
-        fields = '__all__'
-
-    name = forms.TextInput()
-    campaign = forms.ModelMultipleChoiceField(queryset=Campaign.objects.all(), widget=forms.CheckboxSelectMultiple)
-
-
+# from django import forms
+#
+# from .models import Tags
+# from startsmart.campaigns.models import Campaign
+#
+# class TagsForm(forms.ModelForm):
+#     # campaign = forms.ChoiceField(choices=Campaign.objects.all())
+#
+#     def __init__(self, *args, **kwargs):
+#         super(TagsForm, self).__init__(*args, **kwargs)
+#
+#     class Meta:
+#         model = Tags
+#         fields = '__all__'
+#
+#     name = forms.TextInput()
+#     campaigns = forms.ModelMultipleChoiceField(queryset=Campaign.objects.all(), widget=forms.CheckboxSelectMultiple)
+#
+#
