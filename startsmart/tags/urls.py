161380e7d2c8a62c71f7c56ff20f841b0d372168
possipov <possipov@uci.edu>
2020-11-10 21:31:02 -0800

    Removed create tags url in url Tags.

--- startsmart/tags/urls.py
+++ startsmart/tags/urls.py
@@ -2,5 +2,4 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_tags, name='create-tags'),
 path('filter/<str:tagname>', views.filter_campaigns_from_tags, name='filter-campaigns-from-tags')]
