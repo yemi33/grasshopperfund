56572b8f2c4ecedcdf6d3d4f3b5b3c561c134b5c
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-11-18 14:19:43 -0800

    implemented class-based view for creating posts

--- grasshopperfund/posts/urls.py
+++ grasshopperfund/posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('view/<str:organization_name>/<str:post_id>', views.view_post, name='view-post'),
+    path('create/<str:organization_name>', views.CreatePostView.as_view(), name='create-post'),
+
 ]
--- grasshopperfund/posts/views.py
+++ grasshopperfund/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
 from django.contrib import messages
 
 from ..organizations.models import Organization
@@ -30,3 +32,41 @@ def view_post(request, organization_name: str, post_id: str):
     }
 
     return render(request, 'posts/view_post.html', context)
+
+
+
+class CreatePostView(FormView):
+    template_name = 'posts/create_post.html'
+    form_class = PostForm
+    success_url = 'posts/view'
+
+    def post(self, request, organization_name:str):
+        '''
+        Check post request for valid form
+        '''
+        form = self.get_form()
+        if form.is_valid():
+            # get organization
+            self.organization = Organization.objects.get(name=organization_name)
+            messages.success(request, "Posted!")
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        '''
+        This method is called on valid form data on a POST request
+        '''
+        author = form.cleaned_data["author"]
+        text = form.cleaned_data["text"]
+
+        new_post = Post(
+            author = author,
+            organization = self.organization,
+            text = text,
+        )
+
+        new_post.save()
+
+
+        return redirect("view-organization", organization_name = self.organization.name)
