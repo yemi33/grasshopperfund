399b1b95b790bd91adc5cf6345bef0c13d0d459f
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-12-01 12:36:59 -0800

    Tag __str__ only tag name

--- grasshopperfund/tags/models.py
+++ grasshopperfund/tags/models.py
@@ -4,4 +4,4 @@ class Tags(models.Model):
     name = models.CharField(max_length=300, unique=True)
     campaigns = models.ManyToManyField('campaigns.Campaign', related_name='tags', blank=True)
 
-    def __str__(self): return f"Tag name: {self.name}"
+    def __str__(self): return f"{self.name}"
