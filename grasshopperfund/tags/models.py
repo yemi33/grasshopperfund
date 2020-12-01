d5ef9335c7d6569833a796b2355e11398a4457ab
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-11-30 20:53:16 -0800

    implemented backend for interested tags

--- grasshopperfund/tags/models.py
+++ grasshopperfund/tags/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-from ..organizations.models import Organization
-
-# Create your models here.
 class Tags(models.Model):
     name = models.CharField(max_length=300, unique=True)
-    ##organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='tags', null=True)
-    ##post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='tags', null=True)
     campaigns = models.ManyToManyField('campaigns.Campaign', related_name='tags', blank=True)
+    interested_users = models.ManyToManyField('users.Profile', blank=True)
 
-    def __str__(self): return f"#{self.name}"
+    def __str__(self): return f"Tag name: {self.name}"
---
+++ grasshopperfund/users/tests/test_interested_tags.py
@@ -0,0 +1,68 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ...tags.models import Tags
+from ..models import Profile
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.profile = self._create_profile()
+
+        self.tag_names = [
+            "test",
+            "advertising",
+            "gaming"
+        ]
+        self.tags = self._create_tags(self.tag_names)
+
+        # a quick reference to a tag
+        self.tag = self.tags[0]
+
+
+    def _create_profile(self) -> Profile:
+        self.username = "test"
+        self.email = "test@email.com"
+        self.password = "testing#2020"
+        self.user = User.objects.create_user(
+            username = self.username,
+            password = self.password,
+            email=self.email
+        )
+        return Profile.objects.get(user__username = self.username)
+
+
+    def _create_tags(self, tag_names: list) -> list:
+        '''
+        Create tags from a list of tag names
+        '''
+        tags = []
+        for name in tag_names:
+            tag = Tags.objects.create(
+                name=name
+            )
+            tags.append(tag)
+
+        return tags
+
+
+    def test_add_interested_tag(self):
+        '''
+        Test adding an interested tag to a user
+        '''
+        self.profile.interested_tags.add(self.tag)
+
+        assert self.profile.interested_tags is not None
+        assert self.profile.interested_tags.filter(name=self.tag.name).exists()
+
+    def test_add_interested_user(self):
+        '''
+        Test addomg an interested user to a tag
+        '''
+        self.tag.interested_users.add(self.profile)
+        assert self.tag.interested_users.filter(user__username=self.username).exists()
+
+
+    def tearDown(self):
+        # cascade will delete Profile too
+        User.objects.get(username=self.username).delete()
